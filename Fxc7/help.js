3f0278d1e0a42655b8f62d810baf86e830d01d15
Hirusha21 <60113454+Hirusha21@users.noreply.github.com>
2021-01-25 17:28:54 +0530

    Initial commit

---
+++ Fxc7/help.js
@@ -0,0 +1,348 @@
+
+const help = (prefix, ig, name, uptime, pushname2, user, tanggal, jam) => { 
+	return `
+	
+\`\`\`Follow My Instagram\`\`\`
+${ig}
+
+\`\`\`GROUP OFFICIAL\`\`\`
+https://chat.whatsapp.com/GHC5djoQJrcGBJFwYQuQoB
+
+┏━━━━━❬ *USER ${name}* ❭━━━━━━━⊱
+┃╭───────────────────────
+┃├➲ \`\`\`Hai\`\`\`👋 *${pushname2}*
+┣━━━━━━━━━━━━━━━━━
+┃├➲ \`\`\`I am\`\`\` *${name}*
+┃╰───────────────────────
+┃
+┃╭───────────────────────
+┃├➲ \`\`\`Total Pengguna:\`\`\` *${user.length} User*
+┣━━━━━━━━━━━━━━━━━
+┃├➲ \`\`\`Total Donasi:\`\`\` *0%* 🙂
+┃╰───────────────────────
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━━❬ *REGULATION ${name}* ❭━━━━━━━⊱
+┃
+┣⊱❥ \`\`\`AKTIF\`\`\`: ${kyun(uptime)}
+┣⊱❥ \`\`\`JAM\`\`\`: *${jam} WIB*
+┣⊱❥ \`\`\`TANGGAL\`\`\`: *${tanggal}*
+┣⊱❥ \`\`\`VERSION\`\`\`: *5.5 TERMUX*
+┃
+┣⊱❥ ❌ *SPAM*
+┣⊱❥ ❌ *CALL & VC*
+┃ \`\`\`Melanggar??\`\`\` *Banned*
+┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+━━━━━❬ *ABOUT ${name}* ❭━━━━━━⊱
+╭────────────────────────
+├⊱❥ *${prefix}request [Mau Req Fitur Apa?]*
+├⊱❥ *${prefix}report [lapor bug]*
+├⊱❥ *${prefix}info*
+├⊱❥ *${prefix}donasi*
+├⊱❥ *${prefix}owner*
+├⊱❥ *${prefix}speed*
+├⊱❥ *${prefix}daftar*
+├⊱❥ *${prefix}totaluser*
+├⊱❥ *${prefix}chatlist*
+├⊱❥ *${prefix}blocklist*
+├⊱❥ *${prefix}banlist*
+├⊱❥ *${prefix}bahasa*
+╰────────────────────────
+
+
+━━━━━━❬ *MEDIA & DOWNLOAD* ❭━━━━━━━⊱
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭────────────────────────
+├⊱❥ *${prefix}tiktokstalk [username]*
+├⊱❥ *${prefix}igstalk [_farhan_xcode7]*
+├⊱❥ *${prefix}insta [Link]*
+├⊱❥ *${prefix}ssweb [url]*
+├⊱❥ *${prefix}url2img [Url]*
+├⊱❥ *${prefix}tiktok*
+├⊱❥ *${prefix}fototiktok*
+├⊱❥ *${prefix}meme*
+├⊱❥ *${prefix}memeindo*
+├⊱❥ *${prefix}kbbi*
+├⊱❥ *${prefix}wait*
+├⊱❥ *${prefix}trendtwit*
+╰────────────────────────
+
+
+━━━━━━❬ *CREATOR MENU* ❭━━━━━━⊱
+╭────────────────────────
+├⊱❥ *${prefix}stiker*
+├⊱❥ *${prefix}gifstiker*
+├⊱❥ *${prefix}toimg*
+├⊱❥ *${prefix}tomp3*
+├⊱❥ *${prefix}ocr*
+╰─────────────────────────
+
+╭─────────────────────────
+├⊱❥ *${prefix}imoji [❤️]*
+├⊱❥ *${prefix}tts [id Haii]*
+├⊱❥ *${prefix}ttp [Fxc7]*
+├⊱❥ *${prefix}csky [FXC7]*
+├⊱❥ *${prefix}cwooden [FXC7]*
+├⊱❥ *${prefix}ccrossfire [FXC7]*
+├⊱❥ *${prefix}cnaruto [FXC7]*
+├⊱❥ *${prefix}cgbutton [FXC7]*
+├⊱❥ *${prefix}csbutton [FXC7]*
+├⊱❥ *${prefix}cflower [FXC7]*
+├⊱❥ *${prefix}ctxtneon [FXC7]*
+├⊱❥ *${prefix}cflame3d [FXC7]*
+├⊱❥ *${prefix}caglow [FXC7]*
+├⊱❥ *${prefix}cgneon [FXC7]*
+├⊱❥ *${prefix}cthunder [FXC7]*
+├⊱❥ *${prefix}cbokeh [FXC7]*
+├⊱❥ *${prefix}ctoxic [FXC7]*
+├⊱❥ *${prefix}cmatrix [FXC7]*
+├⊱❥ *${prefix}cmusic [FXC7]*
+├⊱❥ *${prefix}cblood [FXC7]*
+├⊱❥ *${prefix}cwater [FXC7]*
+├⊱❥ *${prefix}clava [FXC7]*
+├⊱❥ *${prefix}cfire [FXC7]*
+├⊱❥ *${prefix}cholo [FXC7]*
+├⊱❥ *${prefix}cminion [FXC7]*
+├⊱❥ *${prefix}cold [FXC7]*
+├⊱❥ *${prefix}cneon [FXC7]*
+├⊱❥ *${prefix}csunmory [FXC7]*
+├⊱❥ *${prefix}cbalon [FXC7]*
+├⊱❥ *${prefix}cglue3d [FXC7]*
+├⊱❥ *${prefix}csraved [FXC7]*
+├⊱❥ *${prefix}cswrite [FXC7]*
+├⊱❥ *${prefix}cssummer [FXC7]*
+├⊱❥ *${prefix}ccsky [FXC7]*
+├⊱❥ *${prefix}ccloud [FXC7]*
+├⊱❥ *${prefix}crvintage [FXC7]*
+├⊱❥ *${prefix}cbpink [FXC7]*
+├⊱❥ *${prefix}ctext3d [FXC7]*
+╰──────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}cstune3d [FXC7/BOT]*
+├⊱❥ *${prefix}cspace3d [FXC7/BOT]*
+├⊱❥ *${prefix}cmarvel [FXC7/BOT]*
+├⊱❥ *${prefix}cavengers [FXC7/BOT]*
+├⊱❥ *${prefix}cpubg [FXC7/BOT]*
+├⊱❥ *${prefix}cglitch [FXC7/BOT]*
+├⊱❥ *${prefix}cluxury [FXC7]*
+├⊱❥ *${prefix}cmetal [FXC7/BOT]*
+├⊱❥ *${prefix}cphlogo [FXC7/BOT]*
+├⊱❥ *${prefix}quotemaker [tx/wtrmk/tema]*
+├⊱❥ *${prefix}nulis [nama/kelas/text]*
+╰────────────────────────
+
+
+━━━━━━━❬ *GRUP ONLY* ❭━━━━━━━━⊱
+╭────────────────────
+├⊱❥ *${prefix}modeanime [On/Off]*
+├⊱❥ *${prefix}naruto*
+├⊱❥ *${prefix}minato*
+├⊱❥ *${prefix}boruto*
+├⊱❥ *${prefix}hinata*
+├⊱❥ *${prefix}sakura*
+├⊱❥ *${prefix}sasuke*
+├⊱❥ *${prefix}kaneki*
+├⊱❥ *${prefix}toukachan*
+├⊱❥ *${prefix}rize*
+├⊱❥ *${prefix}akira*
+├⊱❥ *${prefix}itori*
+├⊱❥ *${prefix}kurumi*
+├⊱❥ *${prefix}miku*
+├⊱❥ *${prefix}anime*
+├⊱❥ *${prefix}animecry*
+├⊱❥ *${prefix}neonime*
+├⊱❥ *${prefix}animekiss*
+╰──────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}welcome [On/Off]*
+├⊱❥ *${prefix}grup [buka/tutup]*
+├⊱❥ *${prefix}antilink*
+├⊱❥ *${prefix}ownergrup*
+├⊱❥ *${prefix}setpp*
+├⊱❥ *${prefix}infogc*
+├⊱❥ *${prefix}add*
+├⊱❥ *${prefix}kick*
+├⊱❥ *${prefix}promote*
+├⊱❥ *${prefix}demote*
+├⊱❥ *${prefix}setname*
+├⊱❥ *${prefix}setdesc*
+├⊱❥ *${prefix}linkgrup*
+├⊱❥ *${prefix}tagme*
+├⊱❥ *${prefix}hidetag*
+├⊱❥ *${prefix}tagall*
+├⊱❥ *${prefix}mentionall*
+├⊱❥ *${prefix}fitnah*
+├⊱❥ *${prefix}listadmin*
+├⊱❥ *${prefix}openanime*
+├⊱❥ *${prefix}edotense*
+├⊱❥ *${prefix}kudeta*
+╰──────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}nsfw [On/Off]*
+├⊱❥ *${prefix}nsfwloli*
+├⊱❥ *${prefix}nsfwblowjob*
+├⊱❥ *${prefix}nsfwneko*
+├⊱❥ *${prefix}nsfwtrap*
+├⊱❥ *${prefix}hentai*
+├⊱❥ *${prefix}indohot [MAINTENANCE]
+├⊱❥ *${prefix}bokep [stepMoms]*
+├⊱❥ *${prefix}simih [On/Off]*
+╰────────────────────────
+
+
+━━━━━━❬ *OTHER FUN & GAME* ❭━━━━━⊱
+╭────────────────────────
+├⊱❥ *${prefix}anjing*
+├⊱❥ *${prefix}kucing*
+├⊱❥ *${prefix}testime*
+├⊱❥ *${prefix}hilih*
+├⊱❥ *${prefix}say*
+├⊱❥ *${prefix}apakah*
+├⊱❥ *${prefix}kapankah*
+├⊱❥ *${prefix}bisakah*
+├⊱❥ *${prefix}rate*
+├⊱❥ *${prefix}watak*
+├⊱❥ *${prefix}hobby*
+├⊱❥ *${prefix}infogempa*
+├⊱❥ *${prefix}infonomor*
+├⊱❥ *${prefix}quotes*
+├⊱❥ *${prefix}truth*
+├⊱❥ *${prefix}dare*
+├⊱❥ *${prefix}katabijak*
+├⊱❥ *${prefix}fakta*
+├⊱❥ *${prefix}darkjokes*
+├⊱❥ *${prefix}bucin*
+├⊱❥ *${prefix}pantun*
+╰────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}translate [en/Apa kabar?]*
+├⊱❥ *${prefix}gantengcek [Farhan]*
+├⊱❥ *${prefix}cantikcek [Iriene]*
+├⊱❥ *${prefix}artinama [Farhan]*
+├⊱❥ *${prefix}persengay [Topan]*
+├⊱❥ *${prefix}pbucin [Farhan]*
+├⊱❥ *${prefix}bpfont [Farhan]*
+├⊱❥ *${prefix}textstyle [FXC7]*
+├⊱❥ *${prefix}jadwaltv [antv]*
+├⊱❥ *${prefix}lirik [melukis senja]*
+├⊱❥ *${prefix}chord [Melukis senja]*
+├⊱❥ *${prefix}wiki [Adolf Hitler]*
+├⊱❥ *${prefix}brainly [pertanyaan]*
+├⊱❥ *${prefix}resepmasakan [Tempe]*
+├⊱❥ *${prefix}map [Banyuwangi]*
+├⊱❥ *${prefix}film [Fast and Farious]*
+├⊱❥ *${prefix}pinterest [gambar kucing]*
+├⊱❥ *${prefix}infocuaca [Banyuwangi]*
+├⊱❥ *${prefix}jamdunia [Banyuwangi]*
+├⊱❥ *${prefix}mimpi [Ular]*
+├⊱❥ *${prefix}tinyurl [link]*
+├⊱❥ *${prefix}bitly [link]*
+╰────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}jadwalsholat [Banyuwangi]*
+├⊱❥ *${prefix}quran*
+├⊱❥ *${prefix}quransurah [1]*
+╰────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}encode64 [string]*
+├⊱❥ *${prefix}decode64 [encrypt]*
+╰────────────────────────
+
+╭────────────────────
+├⊱❥ *${prefix}spamcall [083xxxxxxxxx]*
+├⊱❥ *${prefix}spamsms [083xxxxxxxx/jumlah]*
+├⊱❥ *${prefix}spamgmail [blabla@gmail.com/jumlah]*
+╰────────────────────────
+
+
+━━━━━━❬ *OWNER ONLY* ❭━━━━━━━⊱
+╭──────────────────
+├⊱❥ *${prefix}addprem [mentioned]*
+├⊱❥ *${prefix}removeprem [mention]*
+├⊱❥ *${prefix}setppbot*
+├⊱❥ *${prefix}bc*
+├⊱❥ *${prefix}bcgc*
+├⊱❥ *${prefix}ban*
+├⊱❥ *${prefix}unban*
+├⊱❥ *${prefix}block*
+├⊱❥ *${prefix}unblock*
+├⊱❥ *${prefix}clearall*
+├⊱❥ *${prefix}delete*
+├⊱❥ *${prefix}clone*
+├⊱❥ *${prefix}getses*
+├⊱❥ *${prefix}leave*
+╰────────────────────────
+
+
+━━━━━━❬ *PREMIUM ONLY* ❭━━━━━━━⊱
+╭──────────────────
+├⊱❥ *${prefix}playmp3 [MAINTENANCE] *
+├⊱❥ *${prefix}fb [link video]*
+├⊱❥ *${prefix}ytmp3 [link yt]*
+├⊱❥ *${prefix}ytmp4 [link yt]*
+├⊱❥ *${prefix}joox [Monolog Pamungkas]*
+├⊱❥ *${prefix}smule [Link Video Smule]*
+╰────────────────────────
+
+
+━━━━━━❬ *SUPPORT ${name}* ❭━━━━━━⊱
+╭────────────────────────
+│
+├➲ *O BOT*
+├➲ *M. HADI FIRMANSYA*
+├➲ *DELIA AULIA*
+├➲ *KEVIN DAVID*
+╰────────────────────────
+
+
+┏━━━━━━━━❬ *NOTE ${name}* ❭━━━━━━━━━⊱
+┃
+┣⊱❥ \`\`\`Bot ini belum selesai sepenuhnya\`\`\`
+┃ \`\`\`Masih dalam proses pengerjaan\`\`\`
+┃ \`\`\`Jadi masih jarang aktif, dan\`\`\`
+┃ \`\`\`Maaf Jika Ada Menu Yang Error\`\`\`
+┃
+┣⊱❥ \`\`\`Jika Lama Harap Ulangi Command\`\`\`
+┃
+┣⊱❥ *Gunakan Command Tanpa []*
+┃
+┣⊱❥ \`\`\`Dan Jika Mengalami Error\`\`\`
+┃ \`\`\`Harap Lapor Owner Dengan Cara\`\`\`
+┃ *${prefix}report* \`\`\`apa pesan errornya\`\`\`
+┃
+┣⊱❥ \`\`\`Mau Invit Bot?? Donasi Gan,\`\`\`
+┃ \`\`\`Kalo Gamau Donasi Follow Ig\`\`\`
+┃ \`\`\`@_farhan_xcode7\`\`\`
+┃
+┣⊱❥ \`\`\`Kalian Bisa Mempublish Quotes Kalian\`\`\`
+┃ \`\`\`Jika Minat Hubungi Aja Owner Buat\`\`\`
+┃ \`\`\`Mempublish\`\`\`
+┃ \`\`\`Quotes Kalian,\`\`\`
+┃ \`\`\`Dan Makasih Buat Temen" Yg Mau Di Public\`\`\`
+┃ \`\`\`Quotesnya:)\`\`\`
+┃
+┣⊱❥ \`\`\`Kenapa Saya Tambahkan Fitur Premium\`\`\`
+┃ \`\`\`You Know Lah Karna Fitur Tersebut Membutuhkan\`\`\`
+┃ \`\`\` Kuota Banyak Untuk Mendownload Dan Mengirimkan\`\`\`
+┃ \`\`\` Audio/ Video Yang Anda Minta\`\`\`
+┃ \`\`\` Mohon Pengertiannya🙂\`\`\`
+┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+}
+exports.help = help
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
---
+++ README.md
@@ -0,0 +1,219 @@
+### I'm FXC7BOT <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="25px">
+<p align="center">
+<a href="https://github.com/FarhanXCode7"><img src="https://raw.githubusercontent.com/FarhanXCode7/termux-bot-wa/main/src/glitchtext.png"></a>
+</p>
+<br>
+
+
+
+<p align="center">
+<a href="#"><img title="termux-bot-wa" src="https://img.shields.io/badge/-TERMUX--BOT--WA-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://github.com/FarhanXCode7"><img title="Author" src="https://img.shields.io/badge/AUTHOR-FARHAN-orange?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/FarhanXCode7/termux-bot-wa/followers"><img title="Followers" src="https://img.shields.io/github/followers/FarhanXCode7?style=flat-square"></a>
+<a href="https://github.com/FarhanXCode7/termux-bot-wa/network/members"><img title="Forks" src="https://img.shields.io/github/forks/FarhanXCode7/termux-bot-wa?style=flat-square"></a>
+<a href="https://github.com/FarhanXCode7/termux-bot-wa/watchers"><img title="watchers" src="https://img.shields.io/github/watchers/FarhanXCode7/termux-bot-wa?style=flat-square"></a>
+
+</p>
+
+
+<details>
+
+
+
+* [Ovo](08311800241)
+* [Pulsa](08311800241)
+</details>
+
+## Tools
+
+```bash
+> Termux
+> WhatsApp
+> 2 HandPhone
+```
+
+## Install
+Follow The Steps Below!
+
+```bash
+> termux-setup-storage
+(after that tap on permission)
+> pkg install git -y
+> git clone https://github.com/FarhanXCode7/termux-bot-wa
+> cd termux-bot-wa
+> bash install.sh
+> npm audit fix
+> npm start / node index.js
+```
+
+## Features
+
+| NEW USER | YES
+| :---------------------------------------------: | :-----------: |
+|  Register Name And Age origin|✅|
+
+|  CREATOR  |                                           YES |
+| :---------------------------------------------: | :-----------: |
+| Sticker Maker|✅|
+| Sticker Gif Maker|✅|
+| Convert Sticker To Image|✅|
+| Convert Video To MP3|✅|
+| Black Pink Logo Maker|✅|
+| 3D Text Maker|✅|
+| Quote Maker|✅|
+| Water Maker|✅|
+| Fire Text Maker
+| Marvel Logo Maker|✅|
+| Snow Write Maker|✅|
+| Ninja Logo Maker|✅|
+| Logo Wolf Maker|✅|
+| And much more |✅|
+
+| MEDIA | YES |
+| :-----------------: | :-------: |
+| Trend Twit|✅|
+| YT Search|✅|
+
+| EDUCATION | YES |
+| :-----------------: | :-------: |
+| The Meaning Of The Name|✅|
+| Text To Sticker|✅|
+| Nulis Name/class/text|✅|
+| Quotes|✅|
+
+| ASK | YES |
+| :-----------------: | :-------: |
+| Apakah|✅|
+| Kapankah|✅|
+| Bisakah|✅|
+
+| DOWNLOADER | YES |
+| :-----------------: | :-------: |
+| Pinterest Downloader|✅|
+| Ytmp3 Downloader|✅|
+| Ytmp4 Downloader|✅|
+| Joox Downloader|✅|
+| Facebook Downloader|✅|
+
+| MEME | YES |
+| :-----------------: | :-------: |
+| Meme|❌|
+| Meme Indo|✅|
+
+| GROUP | YES |
+| :-----------------: | :-------: |
+| Open Group|✅|
+| Link Group|✅|
+| info Group|✅|
+| Close Group|✅|
+| Promote Member|✅|
+| Demote Member|✅|
+| Hide Tag|✅|
+| Tag All Members|✅|
+| Add Member|✅|
+| Kick Member|✅|
+| Show List Admins|✅|
+| Leave Group|✅|
+| Show Owner Group|✅|
+| welcome New Members|✅|
+| Nsfw|✅|
+
+| SOUND | YES |
+| :-----------------: | :-------: |
+| Text To Speach|✅|
+| Play|✅|
+
+| MUSIC | YES |
+| :-----------------: | :-------: |
+| Music Lyrics|✅|
+| Chord Guitar|✅|
+
+| ISLAM | YES |
+| :-----------------: | :-------: |
+| Qur'an|✅|
+| Qur'an Surah 1,2,3 dll |✅|
+
+| STALK | YES |
+| :-----------------: | :-------: |
+| Instagram Stalk|✅|
+| Tiktok Stalk|✅|
+
+| WIBU | YES |
+| :-----------------: | :-------: |
+| Neonime|✅|
+| Pokemon|✅|
+| Nekonime|✅|
+| Naruto|✅|
+| Loli|✅|
+| Random Shota|✅|
+| Random Waifu|✅|
+| Random Anime|✅|
+| And much more|✅|
+
+| FUN | YES |
+| :-----------------: | :-------: |
+| Kucing|✅|
+| Anjing|✅|
+| Alay|✅|
+| Glitch|✅|
+| hilih|✅|
+| Cek Ganteng|✅|
+| Watak|✅|
+| Random Hobby|✅|
+| Pinterest [Optional] |✅|
+
+| INFORMATION | YES |
+| :-----------------: | :-------: |
+| List Bahasa|✅|
+| Information Weather|✅|
+| KBBI|✅|
+| Fakta|✅|
+| Covid|✅|
+
+| 18+ | YES |
+| :-----------------: | :-------: |
+| Random Hentai|✅|
+| NSFW Neko|✅|
+
+| OWNER | YES |
+| :-----------------: | :-------: |
+| Set pp bot|✅|
+| add premium |✅|
+| remove premium |✅|
+| Set Prefix|✅|
+| Block Member|✅|
+| Broadcast|✅|
+| Group Broadcast|✅|
+| Clear All Chat|✅|
+
+ TENTANG BOT | YES |
+| :-----------------: | :-------: |
+| info|✅|
+| ChatList|❌|
+
+
+## Note
+
+* Dont Forget Stars
+
+* |En| And You can add your own quotes
+* |Ind| Dan Kalian Bisa tambahkan Quotes Kalian
+
+
+## Special Thanks
+
+* Created Bot => [MhankBarBar](https://github.com/MhankBarBar)
+
+
+
+## Group
+
+* <a href="https://chat.whatsapp.com/GHC5djoQJrcGBJFwYQuQoB"><img alt="WhatsApp" src="https://img.shields.io/badge/WhatsApp%20Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"/></a>
+
+---
+
+
